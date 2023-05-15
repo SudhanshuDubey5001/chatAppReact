63b7f82b5933c65d3c87753a0e712450c807a169
Sudhanshu Dubey <sudhanshu@sudhanshus-air.home>
2023-05-15 18:29:06 +0100

    setup amplify and GraphQL

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifychatapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
